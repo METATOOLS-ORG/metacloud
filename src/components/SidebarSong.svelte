cb023cbf0c72c731b977a29659a4f79e0217a033
Litexev <64324176+litevex@users.noreply.github.com>
2026-01-17 10:28:17 +0100

    Newer placeholder user assets

---
+++ src/components/SidebarSong.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import { getCoverAssetUrl } from "$lib/assets";
+	import HeartIcon from "./icons/HeartIcon.svelte";
+	import RepostIcon from "./icons/RepostIcon.svelte";
+    const { song } = $props();
+
+</script>
+<section class="sidebar-song">
+    <img width="36px" height="36px" alt="song cover" src={getCoverAssetUrl(song.coverAssetId)}/>
+    <div class="sidebar-song-split">
+        <a class="noblue" href="#todo">
+            <span>{song.title}</span>
+            <div class="sidebar-song-stats">
+                <div class="sidebar-song-stat">
+                    <HeartIcon/>
+                    <span>{song.likes.length}</span>
+                </div>
+                <div class="sidebar-song-stat">
+                    <RepostIcon/>
+                    <span>12</span>
+                </div>
+            </div>
+        </a>
+    </div>
+</section>
+
+
+<style>
+    .sidebar-song {
+        display: flex;
+        gap: 10px;
+    }
+
+    .sidebar-song a {
+        display: contents;
+    }
+    .sidebar-song-split {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+    }
+
+    .sidebar-song-stats {
+        display: flex;
+        gap: 10px;
+    }
+
+    .sidebar-song-stat {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        color: var(--text-color-info);
+    }
+
+    .sidebar-song a {
+        display: contents;
+    }
+
+    .sidebar-song-stats {
+        display: flex;
+        gap: 10px;
+    }
+
+    .sidebar-song-stat {
+        display: flex;
+        align-items: center;
+        gap: 4px;
+        color: var(--text-color-info);
+    }
+</style>
