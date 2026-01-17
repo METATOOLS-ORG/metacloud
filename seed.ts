fa5ca5e8df3c7ce4e2485f1f3c38c037b41dcaf7
Litexev <64324176+litevex@users.noreply.github.com>
2026-01-17 15:32:56 +0100

    Add admin account creation to seed

--- seed.ts
+++ seed.ts
@@ -2,6 +2,7 @@ import { AssetType, AssetPurpose, PrismaClient } from "@prisma/client";
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs/promises';
+import 'dotenv/config'
 const prisma = new PrismaClient()
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
@@ -10,7 +11,20 @@ const UPLOAD_PATH = join(__dirname, "..", "uploads");
 
 // Create default fields in database
 async function main() {
-    // @todo: create default admin account 
+    if (!process.env.ADMIN_PASSWORD)
+        throw new Error("Environment variable ADMIN_PASSWORD not found, make sure to set up .env");
+
+    if (process.env.ADMIN_PASSWORD == "CHANGE_ME")
+        throw new Error("ADMIN_PASSWORD in .env is still set to the default value CHANGE_ME, please change it");
+
+    prisma.user.create({
+        data: {
+            username: "admin",
+            email: "none", // @todo?
+            passwordHash: process.env.ADMIN_PASSWORD!,
+            displayName: "Admin",
+            bio: "Website administrator",
+    }});
 }
 
 
